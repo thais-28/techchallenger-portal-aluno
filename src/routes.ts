33137e7d3539d4908920a8eea24788afb601f14d
Gab <125615352+gab-szz@users.noreply.github.com>
2025-07-28 22:32:02 -0400

    Remover node_modules do repositório

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import * as PostController from "./controllers/post-controller";
 const router = Router();
 
 router.get("/posts", PostController.getPosts);
-router.get("/posts/:id", PostController.getPostById); // Assuming you want to use the same controller for getting a player by ID
+router.get("/posts/:id", PostController.getPostById);
 router.post("/posts", PostController.createPost);
 router.delete("/posts/:id", PostController.deletePost);
 router.patch("/posts/:id", PostController.updatePost);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express, {json, Request, Response} from "express";
-import createApp from './app';
+import express, { json, Request, Response } from "express";
+import createApp from "./app";
 
 const app = createApp();
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log('❤️ Server running at port http://localhost:${port}');
+  console.log(`❤️ Server running at port http://localhost:${port}`);
 });
